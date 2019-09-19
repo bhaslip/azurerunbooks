2cd107439cdde96c7c62a27d1386b26066840269
bhaslip <haslip.bryan@gmail.com>
2019-09-19 08:39:27 -0600

    Add files via upload

---
+++ removestorageaccountfiles.ps1
@@ -0,0 +1,16 @@
+﻿#Script to delete backup files
+$bacs = Get-ChildItem $location # use "-Filter *.bak" or "-Filter *.trn" for deleting bak or trn files specifically
+$container="Container Name"
+$StorageAccountName="Storage Account Name"
+$StorageAccountKey="Storage Account Key"
+$context = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
+$filelist = Get-AzureStorageBlob -Container $container -Context $context
+foreach ($file in $filelist | Where-Object {$_.LastModified.DateTime -lt ((Get-Date).AddDays(-5))})
+{
+$removefile = $file.Name
+if ($removefile -ne $null)
+{
+Write-Host "Removing file $removefile"
+Remove-AzureStorageBlob -Blob $removeFile -Container $container -Context $context
+}
+}
